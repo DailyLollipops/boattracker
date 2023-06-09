9744a8a84345639f40b6c4316ef2fa0918b1d0bb
DailyLollipops <clarencemadrigal84@gmail.com>
2023-06-09 08:54:45 +0800

    Refactored Blockbox to Tracker

--- database/migrations/2023_06_08_142724_create_trackers_table.php
+++ database/migrations/2023_06_08_142724_create_trackers_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('blockboxs', function (Blueprint $table) {
+        Schema::create('trackers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->foreignID('boat_id')->constrained()->onDelete('cascade');
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('blockboxs');
+        Schema::dropIfExists('trackers');
     }
 };
