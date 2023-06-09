78ff6b72bdab9f5fb4f827c5fc5d6b8d106f5d4e
DailyLollipops <clarencemadrigal84@gmail.com>
2023-06-09 09:32:31 +0800

    Added timestamps; change active boat string to integer

--- database/migrations/2023_06_08_142219_create_settings_table.php
+++ database/migrations/2023_06_08_142219_create_settings_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
     {
         Schema::create('settings', function (Blueprint $table) {
             $table->id();
-            $table->string('active_boat');
+            $table->integer('active_boat');
+            $table->timestamps();
         });
     }
 
