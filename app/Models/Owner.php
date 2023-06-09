8d11324f18c6f467002af8795e8249774db0dfa2
DailyLollipops <clarencemadrigal84@gmail.com>
2023-06-09 08:50:41 +0800

    Fixed typo

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Track extends Model
+class Owner extends Model
 {
     use HasFactory;
     protected $fillable = [
