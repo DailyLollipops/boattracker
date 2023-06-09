5917deffdd94c3ecc5a03b9e1fa090464b27bc10
DailyLollipops <clarencemadrigal84@gmail.com>
2023-06-09 09:27:41 +0800

    Added unique on owner name

--- database/migrations/2023_06_08_142153_create_owners_table.php
+++ database/migrations/2023_06_08_142153_create_owners_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('owners', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('barangay');
             $table->string('contact')->nullable();
             $table->timestamps();
