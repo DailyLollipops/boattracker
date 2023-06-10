03582b4e7a3ac91e4ba1bd036d0fee3bf1ce63fa
DailyLollipops <clarencemadrigal84@gmail.com>
2023-06-11 00:09:50 +0800

    Updated settings table migration

--- database/migrations/2023_06_08_142219_create_settings_table.php
+++ database/migrations/2023_06_08_142219_create_settings_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('settings', function (Blueprint $table) {
             $table->id();
-            $table->integer('contact');
+            $table->string('contact');
             $table->timestamps();
         });
     }
