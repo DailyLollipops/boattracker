775a88b747fb902e82d277f4cf8fc615b5376757
DailyLollipops <clarencemadrigal84@gmail.com>
2023-06-11 22:45:02 +0800

    Fixed incorrect label for attribute

--- resources/views/components/modal/update-phone.blade.php
+++ resources/views/components/modal/update-phone.blade.php
@@ -12,7 +12,7 @@
         <div class="w-full flex flex-col space-y-2 px-9 mb-3">
             <div class="flex flex-col relative">
                 <input type="text" id="update-contact-number" name="contact" placeholder="Enter contact number..." class="w-full flex border border-gray-700 rounded-[7px] outline outline-0 font-sans font-normal leading-tight text-sm text-gray-700 focus:ring-1 focus:outline-none focus:ring-gray-700 rounded-lg text-sm px-4 py-2.5 inline-flex placeholder:text-xs" required="">
-                <label for="update-contact-contact" class="relative absolute -top-12 left-3 w-fit px-1 bg-white font-sans font-normal text-gray-700 text-[11px] leading-tight">
+                <label for="update-contact-number" class="relative absolute -top-12 left-3 w-fit px-1 bg-white font-sans font-normal text-gray-700 text-[11px] leading-tight">
                     Contact Number
                 </label>
             </div>
